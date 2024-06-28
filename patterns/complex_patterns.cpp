77276fce224ed95a86e01f4503d00349ecd602d6
kinish123 <kinishneema7723@gmail.com>
2024-06-29 00:47:56 +0530

    some questions

--- patterns/complex_patterns.cpp
+++ patterns/complex_patterns.cpp
@@ -65,18 +65,36 @@
 //   *****
 //    ***
 //     *
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int row,col;
+//     for(row=5;row>=1;row--){
+//         for(col=1;col<=5-row;col++){
+//             cout<<" ";
+//         }
+//         for(col=1;col<=2*row-1;col++){
+//             cout<<"*";
+//         }
+    
+//         cout<<endl;
+//     }
+// }
+
+// ********
+// ***  ***
+// **    **
+// *      *
+// *      *
+// **    **
+// ***  ***
+// ********
 #include<iostream>
 using namespace std;
-int main(){
+int main()
+{
     int row,col;
-    for(row=5;row>=1;row--){
-        for(col=1;col<=5-row;col++){
-            cout<<" ";
-        }
-        for(col=1;col<=2*row-1;col++){
-            cout<<"*";
-        }
     
-        cout<<endl;
-    }
 }
+
+
